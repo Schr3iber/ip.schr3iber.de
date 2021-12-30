3646c9fae2acd653fda2a41491dcf6ded593d231
Henning Schreiber <github@schr3iber.de>
2021-12-30 16:07:13 +0100

    1.1
    Add reverse DNS
    Add Link to stat.ripe.net
    
    modify IPv4/v6 coloring
    modify IPv6 brackets
    modify Time dispaly

--- index.php
+++ index.php
@@ -64,14 +64,21 @@ p.con_info {
 </head>
 <body>
 <?php
-    
- $ip_user = getenv("REMOTE_ADDR") ;
- $ip_server = getenv("SERVER_ADDR") ;
- //$ip_domain = gethostbyname('gtrvqf0reg99y09l.myfritz.net');
+//getting IPs 
+ $ip_user = getenv("REMOTE_ADDR");
+ $ip_server = getenv("SERVER_ADDR");
 
- $port_user = getenv("REMOTE_PORT") ;
+//getting Ports
+ $port_user = getenv("REMOTE_PORT");
  $port_server = getenv("SERVER_PORT");
 
+//revers DNS with RIPEstar
+ $rDNS_url = "https://stat-ui.stat.ripe.net/data/reverse-dns-ip/data.json?resource=" . $ip_user;
+ $json_rDNS = file_get_contents($rDNS_url);
+ $jsondata = json_decode($json_rDNS);
+ $hostname = $jsondata->data->result[0];
+
+//checking http or https
  if ($port_server == '80'){
     Echo '<span class="mark_red">';
         Echo "unencrypted "; 
@@ -81,46 +88,55 @@ p.con_info {
         Echo "encrypted "; 
     Echo'</span>';
 }
- 
-if (strpos($ip_user, '.')) {
-    Echo '<span class="mark_red">';
+
+//IPv4 red, IPv6 green
+ if (strpos($ip_user, '.')) {
+   Echo '<span class="mark_red">';
         Echo "IPv4";
-    Echo '</span>';
-    Echo " request";
-    Echo '<br>';
-    Echo "from ";
-    Echo '<span class="mark_blue">';
-        Echo  $ip_user . ":" . $port_user;
-    Echo'</span>';
-    Echo "<br>";
-    Echo "to "; 
-    Echo '<span class="mark_blue">';
-        Echo  $ip_server . ":" . $port_server . " (ip.schr3iber.de)";
-    Echo'</span>';
- 
-}
-if (strpos($ip_user, ':')) {
-    Echo " request";
-    Echo '<br>';
-    Echo "from ";
-    Echo '<span class="mark_blue">';
-        Echo  "[".$ip_user . "]:" . $port_user;
-    Echo'</span>';
-    Echo "<br>";
-    Echo "to "; 
-    Echo '<span class="mark_blue">';
-        Echo "[" . $ip_server . ":" . $port_server . "] (ip.schr3iber.de)";
-    Echo'</span>';
- 
-}
+ }else{
+    Echo '<span class="mark_green">';
+        Echo "IPv6";
+ }
+ Echo '</span>';
 
-Echo "<br>";
-Echo "at ";
-Echo '<span class="mark_blue">';
-    Echo date('l') . ", " . gmdate('Y-m-d h:i:s \G\M\T');
-Echo'</span>';
+
+//brackets for IPv6
+ $brackets1 = "[";
+ $brackets2 = "]";
+
+ if (strpos($ip_user, '.')) {
+     $brackets1 = "";
+     $brackets2 = "";
+ }
+
+//output
+ Echo " request";
+ Echo '<br>';
+ Echo "from ";
+ Echo '<span class="mark_blue">';
+    Echo  $brackets1 . '<a href="https://stat.ripe.net/app/launchpad/S1_' . $ip_user . '_C13C31C24C20C15C6C7C1C14C27C25C18C29C30C17C2C21C16C11C10">' . $ip_user . '</a>' . $brackets2 . ":" . $port_user;
+ Echo'</span>';
+ Echo '<span class="mark_green">';
+    Echo " (" . $hostname . ")";
+ Echo'</span>';
+
+ Echo "<br>";
+
+ Echo "to "; 
+ Echo '<span class="mark_blue">';
+    Echo $brackets1 . $ip_server . ":" . $port_server . $brackets2;
+ Echo'</span>';
+ Echo '<span class="mark_green">';
+    Echo " (ip.schr3iber.de)";
+ Echo'</span>';
+
+//Date
+ Echo "<br>";
+ Echo '<span class="mark_blue">';
+    date_default_timezone_set("Europa/London");
+    Echo date("l, Y-m-d H:i:s") . " UTC";
+ Echo'</span>';
 ?>
-
-<p>v1.0</p>
+<p>v1.1</p>
 </body>
 </html>
